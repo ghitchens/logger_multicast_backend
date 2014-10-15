b3504a4447ce42b86db310a6035d0e907a97b1b7
Garth Hitchens <ghitchens@me.com>
2014-10-15 14:04:25 -0700

    added test for second line after port open

--- test/logger_multicast_backend_test.exs
+++ test/logger_multicast_backend_test.exs
@@ -6,6 +6,7 @@ defmodule LoggerMulticastBackendTest do
   test "logging doesn't crash" do
     Logger.add_backend LoggerMulticastBackend
     Logger.debug "this should write to the multicast socket!  make sure it does"
+    Logger.debug "this should write more.  make sure it does"
   end
   
 end
